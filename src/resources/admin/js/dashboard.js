f8bf84db2cf273a58dfe3501e64c892c91e6cfeb
phuoc <huuphuocit0201@gmail.com>
2023-03-29 00:10:13 +0700

    #28 - [ADMIN][FEATURE] fix comment

--- src/resources/admin/js/dashboard.js
+++ src/resources/admin/js/dashboard.js
@@ -4,67 +4,13 @@ $(document).ready(function(){
   console.log(days, parameters);
     $(function () {
       //-------------
-      //- DONUT CHART -
-      //-------------
-      // Get context with jQuery - using jQuery's .get() method.
-
-      // var donutData        = {
-      //   labels: [
-      //       'Chrome',
-      //       'IE',
-      //       'FireFox',
-      //       'Safari',
-      //       'Opera',
-      //       'Navigator',
-      //   ],
-      //   datasets: [
-      //     {
-      //       data: [700,500,400,600,300,100],
-      //       backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
-      //     }
-      //   ]
-      // }
-
-      //-------------
-      //- PIE CHART -
-      //-------------
-      // Get context with jQuery - using jQuery's .get() method.
-
-      // var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
-      // var pieData        = donutData;
-      // var pieOptions     = {
-      //   maintainAspectRatio : false,
-      //   responsive : true,
-      // }
-
-      //Create pie or douhnut chart
-      // You can switch between pie and douhnut using the method below.
-
-      // new Chart(pieChartCanvas, {
-      //   type: 'pie',
-      //   data: pieData,
-      //   options: pieOptions
-      // })
-  
-      //-------------
       //- BAR CHART -
       //-------------
       var areaChartData = {
         labels  : days,
         datasets: [
-          // {
-          //   label               : 'Digital Goods',
-            // backgroundColor     : 'rgba(60,141,188,0.9)',
-            // borderColor         : 'rgba(60,141,188,0.8)',
-          //   pointRadius          : false,
-          //   pointColor          : '#3b8bba',
-          //   pointStrokeColor    : 'rgba(60,141,188,1)',
-          //   pointHighlightFill  : '#fff',
-          //   pointHighlightStroke: 'rgba(60,141,188,1)',
-          //   data                : [28, 48, 40, 19, 86, 27, 90]
-          // },
           {
-            label               : 'Electronics',
+            label               : 'doanh thu',
             backgroundColor     : 'rgba(60,141,188,0.9)',
             borderColor         : 'rgba(60,141,188,0.8)',
             pointRadius         : false,
