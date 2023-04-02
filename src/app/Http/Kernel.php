2ed9032fd02a1458d479e0d9affbe3916f74271f
nguyen <han16052001@gmail.com>
2023-04-02 12:59:09 +0700

    #32 - [USER][FEATURE] authentication user

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -54,6 +54,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth.admin' => \App\Http\Middleware\AdminAuthenticate::class,
+        'auth.user' => \App\Http\Middleware\UserAuthenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
